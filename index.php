9459989cde6efc5cf987af9138d48d19c88b97e6
Bemek00 <patrykbohm00@gmail.com>
2020-11-29 11:39:56 +0100

    change

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title> Optical Fiber Documentation </title>
+        <link rel="stylesheet" href="index.css" type="text/css" />
+    </head>
+    <body>
+        <div class="screen">
+            <div class="menu">
+
+            </div>
+            <div class="naglowek">
+
+            </div>
+            <div class="content">
+
+            </div>
+        </div>
+    </body> 
+</html> 
